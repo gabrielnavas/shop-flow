25adf23e41fb421de0dd894738f650dc58eae58d
navas <gabrielnavas@protonmail.com>
2025-01-25 10:09:16 -0300

    add styled compoenents

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
 import { useEffect } from "react"
+import styled from "styled-components"
 
 export const App = () => {
   useEffect(() => {
     document.title="Hello World!"
   }, [])
-  
+
   return (
-    <div>Hello World</div>
+    <Container>Hello World</Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  color: green;
+  font-weight: bold;
+  font-size: 80px;
+`
