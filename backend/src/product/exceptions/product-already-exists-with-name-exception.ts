453c2ac6ba757c9cc6093c2c9c5dbcbd0869d9e4
navas <gabrielnavas@protonmail.com>
2025-02-01 18:23:11 -0300

    fix message

--- backend/src/product/exceptions/product-already-exists-with-name-exception.ts
+++ backend/src/product/exceptions/product-already-exists-with-name-exception.ts
@@ -1,6 +1,6 @@
 export class ProductAlreadyExistsWithNameException extends Error {
   constructor(name: string) {
-    super(`Producto já existe com o nome: ${name}`);
+    super(`Produto já existe com o nome: ${name}`);
     this.name = 'ProductAlreadyExistsWithNameException';
   }
 }
