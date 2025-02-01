3e389a947589aaa28ad8f538a7a89e1ef6045091
navas <gabrielnavas@protonmail.com>
2025-02-01 18:23:27 -0300

    remove complex component

--- frontend/src/components/ui/Form.tsx
+++ frontend/src/components/ui/Form.tsx
@@ -1,19 +1,6 @@
 import styled from "styled-components"
 
-type Props = {
-  children: React.ReactNode
-  onSubmit?: React.FormEventHandler<HTMLFormElement> | undefined
-}
-
-export const Form = ({ onSubmit, children }: Props) => {
-  return (
-    <Container onSubmit={onSubmit}>
-      {children}
-    </Container>
-  )
-}
-
-const Container = styled.form`
+export const Form = styled.form`
   width: 100%;
   display: flex;
   flex-direction: column;
